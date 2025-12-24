db7066bf6c7b27e85b92f160f6b6a34774243902
Clare <yuhsin099@gmail.com>
2025-12-24 23:53:31 +0800

    create home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,138 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="logo">Le Savor 西饗</div>
+    <div class="function-frame">
+      <router-link to="/">主頁</router-link>
+      <router-link to="/about">關於我們</router-link>
+      <router-link to="/menu">菜單</router-link>
+      <router-link to="/review">評論</router-link>
+      <router-link to="/contact">客服中心</router-link>
+    </div>
+    <div class="icon-frame">
+      <router-link to="/cart">
+        <i class="fa-solid fa-cart-shopping"></i>
+      </router-link>
+      <router-link to="/user">
+        <i class="fa-solid fa-user"></i>
+      </router-link>
+      <router-link to="/logout">
+        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+      </router-link>
+    </div>
   </nav>
+  <div class="content-frame">
+    <div class="text-frame">
+      <div class="slogan">細細品味西式料理的藝術。</div>
+      <div class="sec-slogan">精緻餐飲，應該成為日常生活的一部分。</div>
+      <div class="content">
+        我們嚴選高品質食材，以主廚等級的專業手藝精心製作每一道料理，<br />
+        讓您無論何時、在家中也能輕鬆享受餐廳級的西式饗宴。
+      </div>
+      <div class="btn">了解更多</div>
+    </div>
+    <img src="@/assets/homepage2.png" alt="Logo" />
+  </div>
   <router-view />
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Brush Script MT", cursive; // 字體設定
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #272727;
+
+  width: 100vw;
+  height: 100vh;
+  background-image: url("@/assets/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  overflow-x: hidden;
 }
 
+body {
+  // 周圍不留空白
+  margin: 0;
+  padding: 0;
+}
 nav {
-  padding: 30px;
+  padding: 20px 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.logo {
+  font-size: 35px;
+  font-weight: bold;
+}
+.function-frame a {
+  color: #272727;
+  font-size: 24px;
+  font-weight: bold;
+  text-decoration: none;
+  margin: 0 20px;
+  display: inline-block; // 讓動畫生效
+  transition: all 0.3s ease;
+}
+.function-frame a:hover {
+  color: #f0c42d;
+  transform: scale(1.1);
+}
+.icon-frame i {
+  color: #272727;
+  font-size: 18px;
+  margin: 0 10px;
+  display: inline-block; // 讓動畫生效
+  transition: all 0.3s ease;
+}
+.icon-frame i:hover {
+  color: #f0c42d;
+  transform: scale(1.1);
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.content-frame {
+  padding: 20px 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.text-frame {
+  width: 40%;
+  text-align: left;
+}
+.slogan {
+  font-size: 34px;
+  font-weight: bold;
+}
+.sec-slogan {
+  color: #f0c42d;
+  font-size: 26px;
+  margin: 10px 0;
+}
+.btn {
+  width: 150px;
+  height: 40px;
+  color: #ffffff;
+  font-size: 20px;
+  text-align: center;
+  line-height: 40px;
+  // background-color: #d7bd88;
+  background-color: #f0c42d;
+  border-radius: 20px;
+  margin-top: 20px;
+  transition: all 0.3s ease;
+}
+.btn:hover {
+  color: #272727;
+  background-color: #ffea9d;
+  cursor: pointer;
+  transform: scale(1.1);
+}
+img {
+  width: 600px;
+  height: auto;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,149 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="title">熱門菜色</div>
+    <div class="dish-info-frame">
+      <img src="@/assets/homepage3.png" />
+      <div class="text-frame">
+        <div class="dish-name">茄汁肉醬寬扁麵</div>
+        <div class="dish-description">
+          以新鮮番茄熬製的濃郁肉醬，搭配彈牙的寬扁麵，帶來豐富的口感與層次。
+        </div>
+        <div class="dish-stars-title">
+          推薦指數
+          <span class="dish-stars" v-for="i in 5" :key="i">
+            <i class="fa-solid fa-star"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="dish-info-frame">
+      <div class="text-frame">
+        <div class="dish-name">青醬貝殼義大利麵</div>
+        <div class="dish-description">
+          以新鮮羅勒和松子製成的青醬，搭配貝殼義大利麵，風味獨特。
+        </div>
+        <div class="dish-stars-title">
+          推薦指數
+          <span class="dish-stars" v-for="i in 4" :key="i">
+            <i class="fa-solid fa-star"></i>
+          </span>
+        </div>
+      </div>
+      <img src="@/assets/homepage4.png" />
+    </div>
+  </div>
+  <div class="home">
+    <div class="title">Why Le Savor 西饗 ?</div>
+    <div class="info-box-frame">
+      <div class="info-box">
+        <i class="fa-solid fa-utensils"></i>
+        <div class="info-title">主廚等級的料理工藝</div>
+        <div class="info-description">
+          我們的料理由經驗豐富的主廚精心製作，確保每一道菜色都達到最高品質標準。
+        </div>
+      </div>
+      <div class="info-box">
+        <i class="fa-solid fa-leaf"></i>
+        <div class="info-title">嚴選頂級食材來源</div>
+        <div class="info-description">
+          我們只選用通過品質認證的進口肉品、新鮮蔬菜與天然香料，堅持不使用來路不明或過度加工的食材，為您的餐桌把關每一口安心。
+        </div>
+      </div>
+      <div class="info-box">
+        <i class="fa-solid fa-snowflake"></i>
+        <div class="info-title">冷鏈直送鎖住新鮮</div>
+        <div class="info-description">
+          全程低溫配送，從出餐到您手中皆維持最佳保存狀態，完美鎖住鮮度與風味，確保餐點到家仍如剛出爐般美味。
+        </div>
+      </div>
+    </div>
+    <div class="info-box-frame">
+      <div class="info-box">
+        <i class="fa-solid fa-gift"></i>
+        <div class="info-title">儀式感為日常而生</div>
+        <div class="info-description">
+          Le Savor
+          相信用餐不只是填飽肚子，我們為每一道料理設計專屬擺盤與份量比例，讓平凡的夜晚，也能變成值得記住的饗宴時刻。
+        </div>
+      </div>
+      <div class="info-box">
+        <i class="fa-solid fa-earth-asia"></i>
+        <div class="info-title">用心對待地球</div>
+        <div class="info-description">
+          支持在地小農與友善飼養，使用環保包裝材料，讓美味與責任同時被端上餐桌。
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  padding: 0 50px;
+}
+.title {
+  color: #f0c42d;
+  font-size: 30px;
+  font-weight: bold;
+  padding-top: 50px;
+}
+.dish-info-frame {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0;
+}
+.dish-name {
+  font-size: 26px;
+  margin: 10px 0;
+}
+.dish-stars-title {
+  margin: 10px 0;
+}
+i {
+  color: #f0c42d;
+  font-size: 20px;
+  margin: 0 3px;
+}
+img {
+  width: 250px;
+  margin: 0 20px;
+}
+.info-box-frame {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0;
+}
+.info-box {
+  width: 30%;
+  height: 200px;
+  text-align: center;
+  margin: 0 10px;
+  padding: 20px;
+  background-color: #fdf8e1;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.info-box i {
+  font-size: 40px;
+  margin-bottom: 10px;
+}
+.info-title {
+  font-size: 26px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+</style>
